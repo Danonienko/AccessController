dd65e3f2e85ff54a3e85a6fe9087ed46d5bc23e8
Daniel <danonienko8@gmail.com>
2025-08-25 14:28:48 +0300

    Breaking: Make `ClearanceController` abstract
    - Make all methods static

--- src/ClearanceController.d.ts
+++ src/ClearanceController.d.ts
@@ -1,16 +1,3 @@
-declare interface IClearanceController {
-	GetPlayerClearanceLevel(player: Player): number;
-	GetPlayerKeyCards(player: Player): KeyCard[];
-
-	GetGatekeeperClearanceLevel(gatekeeper: Gatekeeper): number;
-	GetGatekeeperKeyCards(gatekeeper: Gatekeeper): string[];
-
-	PlayerHasClearance(player: Player, gatekeeper: Gatekeeper): boolean;
-
-	IsAKeyCard(tool: unknown): tool is KeyCard;
-	IsAGatekeeper(instance: unknown): instance is Gatekeeper;
-}
-
 declare type KeyCardConfig = Configuration & {
 	Level: NumberValue;
 };
--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,12 @@ import { t } from "@rbxts/t";
 import Constants from "Constants";
 import Validators from "Validators";
 
-export default class ClearanceController implements IClearanceController {
-	protected _getPlayerBackpackContents(player: Player): Instance[] {
+export default abstract class ClearanceController {
+	protected static _getPlayerBackpackContents(player: Player): Instance[] {
 		return player.FindFirstChildOfClass("Backpack")?.GetChildren() ?? [];
 	}
 
-	public IsAKeyCard(tool: unknown): tool is KeyCard {
+	public static IsAKeyCard(tool: unknown): tool is KeyCard {
 		if (!t.instanceIsA("Tool")(tool)) return false;
 		if (!tool.HasTag(Constants.KEY_CARD_TAG)) return false;
 		if (!Validators.KeyCardValidator(tool)) return false;
@@ -15,7 +15,7 @@ export default class ClearanceController implements IClearanceController {
 		return true;
 	}
 
-	public IsAGatekeeper(instance: unknown): instance is Gatekeeper {
+	public static IsAGatekeeper(instance: unknown): instance is Gatekeeper {
 		if (!t.instanceIsA("Instance")(instance)) return false;
 		if (!instance.HasTag(Constants.GATEKEEPER_TAG)) return false;
 		if (!Validators.GatekeeperValidator(instance)) return false;
@@ -24,7 +24,7 @@ export default class ClearanceController implements IClearanceController {
 	}
 
 	/** Returns highest clearance level the player has */
-	public GetPlayerClearanceLevel(player: Player): number {
+	public static GetPlayerClearanceLevel(player: Player): number {
 		let highestLevel = 0;
 
 		for (const tool of this._getPlayerBackpackContents(player)) {
@@ -37,7 +37,7 @@ export default class ClearanceController implements IClearanceController {
 	}
 
 	/** Returns an array of keycard tool instances the player has in their backpack */
-	public GetPlayerKeyCards(player: Player): KeyCard[] {
+	public static GetPlayerKeyCards(player: Player): KeyCard[] {
 		const keyCards: KeyCard[] = [];
 
 		for (const tool of this._getPlayerBackpackContents(player)) {
@@ -49,7 +49,7 @@ export default class ClearanceController implements IClearanceController {
 	}
 
 	/** Returns the clearance level of the gatekeeper instance */
-	public GetGatekeeperClearanceLevel(gatekeeper: Gatekeeper): number {
+	public static GetGatekeeperClearanceLevel(gatekeeper: Gatekeeper): number {
 		if (!this.IsAGatekeeper(gatekeeper)) {
 			warn("Invalid gatekeeper provided in 'GetGatekeeperClearanceLevel'");
 			return 0;
@@ -59,7 +59,7 @@ export default class ClearanceController implements IClearanceController {
 	}
 
 	/** Returns an array of KeyCards the gatekeeper accepts */
-	public GetGatekeeperKeyCards(gatekeeper: Gatekeeper): string[] {
+	public static GetGatekeeperKeyCards(gatekeeper: Gatekeeper): string[] {
 		if (!this.IsAGatekeeper(gatekeeper)) {
 			warn("Invalid gatekeeper provided in 'GetGatekeeperKeyCards'");
 			return [];
@@ -77,7 +77,7 @@ export default class ClearanceController implements IClearanceController {
 	}
 
 	/** Returns a boolean indicating wether or not a player has clearance */
-	public PlayerHasClearance(player: Player, gatekeeper: Gatekeeper): boolean {
+	public static PlayerHasClearance(player: Player, gatekeeper: Gatekeeper): boolean {
 		if (this.GetPlayerClearanceLevel(player) >= this.GetGatekeeperClearanceLevel(gatekeeper)) return true;
 
 		const gatekeeperCards = this.GetGatekeeperKeyCards(gatekeeper);
