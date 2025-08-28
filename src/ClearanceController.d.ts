56fa6e4e5e022abcd91e6bdf4bb378f7e0fc0d52
Daniel <danonienko8@gmail.com>
2025-08-28 21:55:29 +0300

    style: Sort types

--- src/ClearanceController.d.ts
+++ src/ClearanceController.d.ts
@@ -1,16 +1,16 @@
+declare type KeyCard = Tool & {
+	KeyCardConfig: KeyCardConfig;
+};
+
 declare type KeyCardConfig = Configuration & {
 	Level: NumberValue;
+};
+
+declare type Gatekeeper = Instance & {
+	GatekeeperConfig: GatekeeperConfig;
 };
 
 declare type GatekeeperConfig = Configuration & {
 	Clearance: NumberValue;
 	KeyCards: Folder;
-};
-
-declare type KeyCard = Tool & {
-	KeyCardConfig: KeyCardConfig;
-};
-
-declare type Gatekeeper = Instance & {
-	GatekeeperConfig: GatekeeperConfig;
 };
